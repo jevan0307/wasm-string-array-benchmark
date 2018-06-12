8db5e3200b98dd1e5a232075ca00f1a2d2b70a02
Jevan Chan <jevan.cnchan@gmail.com>
2018-06-12 23:13:56 +0800

    Add benchmark result

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # wasm-string-array-benchmark
+
+## Passing string array to WebAssembly
+Sample function: `stringCat`   
+String lentgh: 20
+
+| Method\Number of strings | 10000 | 100000 | 1000000 |
+| ------------------------ | ----- | ------ | ------- |
+| JSON string              | 2ms     | 21ms     | 258ms   |
+| Intermediate Object (wasm-bindgen) | 8ms | 84ms | 887ms |
+
+## Returning string array from WebAssembly
+Sample function: `stringSplit`   
+String lentgh: 20
+
+| Method\Number of strings | 10000 | 100000 | 1000000 |
+| ------------------------ | ----- | ------ | ------- |
+| JSON string              | 8ms     | 114ms     | 1161ms    |
+| Intermediate Object (wasm-bindgen) | 16ms | 168ms | 1814ms |
--- benchmark/index.js
+++ benchmark/index.js
@@ -1,59 +1,82 @@
 const StringUtils = require("../wasm-api");
 const Benchmark = require("benchmark");
 const benchmarkOptions = {
-	onCycle: function() {
-		process.stderr.write(".");
-	},
-	minSamples: 10
+    onCycle: function() {
+        process.stderr.write(".");
+    },
+    minSamples: 10
 };
 
 const StringLens = [20];
 const StringNums = [10000, 100000, 1000000];
-const suite = new Benchmark.Suite;
+const suite = new Benchmark.Suite();
 
-StringLens.forEach((len) => {
-	StringNums.forEach((num) => {
-		let bigString = ("a".repeat(len) + "\n").repeat(num);
-		suite.add(`StringSplit(JSON): Len: ${len}, Num: ${num}`, () => {
-			let x = StringUtils.stringSplitJson(bigString);
-		}, benchmarkOptions)
-	})
-})
+StringLens.forEach(len => {
+    StringNums.forEach(num => {
+        let bigString = ("a".repeat(len) + "\n").repeat(num);
+        suite.add(
+            `StringSplit(JSON): Len: ${len}, Num: ${num}`,
+            () => {
+                let x = StringUtils.stringSplitJson(bigString);
+            },
+            benchmarkOptions
+        );
+    });
+});
 
-StringLens.forEach((len) => {
-	StringNums.forEach((num) => {
-		let bigString = ("a".repeat(len) + "\n").repeat(num);
-		suite.add(`StringSplit(Intermediate Object): Len: ${len}, Num: ${num}`, () => {
-			let x = StringUtils.stringSplit(bigString);
-		}, benchmarkOptions)
-	})
-})
+StringLens.forEach(len => {
+    StringNums.forEach(num => {
+        let bigString = ("a".repeat(len) + "\n").repeat(num);
+        suite.add(
+            `StringSplit(Intermediate Object): Len: ${len}, Num: ${num}`,
+            () => {
+                let x = StringUtils.stringSplit(bigString);
+            },
+            benchmarkOptions
+        );
+    });
+});
 
-StringLens.forEach((len) => {
-	StringNums.forEach((num) => {
-		let bigStringArray = Array(num).fill("a".repeat(len));
-		suite.add(`StringCat(JSON): Len: ${len}, Num: ${num}`, () => {
-			let x = StringUtils.stringCatJson(bigStringArray);
-		}, benchmarkOptions)
-	})
-})
+StringLens.forEach(len => {
+    StringNums.forEach(num => {
+        let bigStringArray = Array(num).fill("a".repeat(len));
+        suite.add(
+            `StringCat(JSON): Len: ${len}, Num: ${num}`,
+            () => {
+                let x = StringUtils.stringCatJson(bigStringArray);
+            },
+            benchmarkOptions
+        );
+    });
+});
 
-StringLens.forEach((len) => {
-	StringNums.forEach((num) => {
-		let bigStringArray = Array(num).fill("a".repeat(len));
-		suite.add(`StringCat(Intermediate Object): Len: ${len}, Num: ${num}`, () => {
-			let x = StringUtils.stringCat(bigStringArray);
-		}, benchmarkOptions)
-	})
-})
-
+StringLens.forEach(len => {
+    StringNums.forEach(num => {
+        let bigStringArray = Array(num).fill("a".repeat(len));
+        suite.add(
+            `StringCat(Intermediate Object): Len: ${len}, Num: ${num}`,
+            () => {
+                let x = StringUtils.stringCat(bigStringArray);
+            },
+            benchmarkOptions
+        );
+    });
+});
 
 suite.on("cycle", event => {
-	process.stderr.write("\n");
-	const b = event.target;
-	console.log(b.name + "\t" + Math.floor(1000 * (b.stats.mean - b.stats.moe)) + "\t" + Math.floor(1000 * (b.stats.mean + b.stats.moe)));
+    process.stderr.write("\n");
+    const b = event.target;
+    console.log(
+        b.name +
+            "\t" +
+            Math.floor(1000 * b.stats.mean) +
+            "\t" +
+            Math.floor(1000 * (b.stats.mean - b.stats.moe)) +
+            "\t" +
+            Math.floor(1000 * (b.stats.mean + b.stats.moe))
+    );
 });
 
 suite.run({
-	async: true
+    async: true
 });
